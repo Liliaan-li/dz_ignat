bebf0b4fbfd6459256ef902f57e564d95e2df22a
Liliaan-li <liliya.kop2003@gmail.com>
2022-03-02 21:22:56 +0300

    First commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+export type MessageProps ={
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+function Message(props: MessageProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
