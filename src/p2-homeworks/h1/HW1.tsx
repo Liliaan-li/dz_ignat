e0290222ad87c91c48051462b6cc4b13272d1e9b
Liliaan-li <liliya.kop2003@gmail.com>
2022-03-03 18:56:05 +0300

    first done homework

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -4,7 +4,7 @@ import Message from "./Message";
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Lilka',
-    message: 'Helooo everyone ',
+    message: 'Helloo everyone:) ',
     time: '13:45',
 }
 
