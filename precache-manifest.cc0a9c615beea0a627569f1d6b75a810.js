16a2b977a0a4f4250dcd8f77df9bc2645faee3a4
Liliaan-li <liliya.kop2003@gmail.com>
2022-04-03 00:22:58 +0300

    Updates

---
+++ precache-manifest.cc0a9c615beea0a627569f1d6b75a810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bf16b0f5a90f496289e05d99d8416f",
+    "url": "/dz_ignat/index.html"
+  },
+  {
+    "revision": "d1846bd9b7165456dda1",
+    "url": "/dz_ignat/static/css/main.bd3b7b01.chunk.css"
+  },
+  {
+    "revision": "53967ac457ad88a3f524",
+    "url": "/dz_ignat/static/js/2.2163938c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dz_ignat/static/js/2.2163938c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1846bd9b7165456dda1",
+    "url": "/dz_ignat/static/js/main.740a5093.chunk.js"
+  },
+  {
+    "revision": "2cb403ceec090294cf7d",
+    "url": "/dz_ignat/static/js/runtime-main.bc83b02e.js"
+  }
+]);
